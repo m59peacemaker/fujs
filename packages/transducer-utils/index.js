0a9e8a7ea079f21f182ae67877ec62f2aa08da3e
Johnny Hauser <johnnyhauser@gmail.com>
2017-04-04 12:38:59 -0500

    "flatten" was wrong and a mess, moved it out for now"

--- packages/transducer-utils/index.js
+++ packages/transducer-utils/index.js
@@ -7,35 +7,3 @@
   result: (finalValue) => transformedFinalValue
   step: (result, input) => resultOfNextStep or Reduced (ordinary value or type Reduced)
  */
-
-
-import protocol from 'transducer-protocol'
-import srslyObject from '../is/srsly-object'
-import curryNCore from '../curry-n/core'
-
-const preservingReduced = nextStep => (acc, v) => {
-  const result = nextStep(acc, v)
-  return isReduced(result) ? Reduced(result) : result
-}
-
-const flatten = SimpleTransducer(nextStep => {
-  return (result, input) => reduce(preservingReduced(nextStep), result, input)
-})
-
-export {
-  transduce,
-  into,
-  reduce,
-  compose,
-  map,
-  filter,
-  flatten,
-  Xf,
-  Reduced,
-  xfProps,
-  normalizeXf,
-  isXf,
-  isReduced,
-  unreduced,
-  deref
-}
